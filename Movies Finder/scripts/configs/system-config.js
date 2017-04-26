71986f22079a6be71b38debf95fe806cede465ea
PetarYanev <yanev.petar@gmail.com>
2017-04-26 18:52:23 +0300

    Add files via upload

--- Movies Finder/scripts/configs/system-config.js
+++ Movies Finder/scripts/configs/system-config.js
@@ -1,3 +1,5 @@
+/*globals SystemJS*/
+
 SystemJS.config({
     // tell SystemJS which transpiler to use
     transpiler: "plugin-babel",
@@ -6,6 +8,13 @@ SystemJS.config({
         "plugin-babel": "../../node_modules/systemjs-plugin-babel/plugin-babel.js",
         "systemjs-babel-build": "../../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js",
         // app start script
-        "app": "./scripts/app.js"
+        "app": "./scripts/app.js",
+        "sammy": "./node_modules/sammy/lib/sammy.js",
+        "jquery": "./node_modules/jquery/dist/jquery.min.js",
+        "handlebars": "./node_modules/handlebars/dist/handlebars.js",
+        "template": "./scripts/template.js",
+        "account-controller": "./scripts/controllers/account-controller.js"
     }
 });
+
+System.import("app");
