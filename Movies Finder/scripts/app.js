1f666b6a79675d8e8aac484be29314d3cb9fd3e4
PetarYanev <yanev.petar@gmail.com>
2017-05-01 09:51:13 +0300

    add more sorting categories

--- Movies Finder/scripts/app.js
+++ Movies Finder/scripts/app.js
@@ -4,62 +4,62 @@ import { template } from "template";
 import { accountControl } from "account-controller";
 import { galleyControl } from "gallery-controller";
 
-let router = Sammy("#content", function () {
+let router = Sammy("#content", function() {
     let $content = $("#content");
 
-    this.get("#/home/", function () {
+    this.get("#/home/", function() {
         galleyControl.getAllMovies()
-            .then(function (movies) {
+            .then(function(movies) {
                 template.get("home")
-                    .then(function (template) {
+                    .then(function(template) {
                         $content.html(template(movies));
                     });
             });
 
     });
 
-    this.get("#/login", function (context) {
+    this.get("#/login", function(context) {
         if (accountControl.currentUser()) {
             context.redirect("#/home/");
             return;
         }
 
         template.get("login")
-            .then(function (template) {
+            .then(function(template) {
                 $content.html(template());
 
-                $("#btn-login").on("click", function () {
+                $("#btn-login").on("click", function() {
                     let loginUser = {
                         username: $("#tb-username").val(),
                         password: $("#tb-password").val()
                     };
 
                     accountControl.userLogin(loginUser)
-                        .then(function () {
+                        .then(function() {
                             context.redirect("#/home/");
                         });
                 });
             });
     });
 
-    this.get("#/register", function (context) {
+    this.get("#/register", function(context) {
         if (accountControl.currentUser()) {
             context.redirect("#/login");
             return;
         }
 
         template.get("register")
-            .then(function (template) {
+            .then(function(template) {
                 $content.html(template());
 
-                $("#btn-register").on("click", function () {
+                $("#btn-register").on("click", function() {
                     let registerNewUser = {
                         username: $("#tb-regUsername").val(),
                         password: $("#tb-regPassword").val()
                     };
 
                     accountControl.userRegister(registerNewUser)
-                        .then(function () {
+                        .then(function() {
                             if (registerNewUser.username.trim() === "" || registerNewUser.password.trim() === "") {
                                 // toastr.error("Invalid username or password");
                             } else {
@@ -70,26 +70,46 @@ let router = Sammy("#content", function () {
             });
     });
 
-    this.get("#/logout", function (context) {
+    this.get("#/logout", function(context) {
         accountControl.userLogout();
         context.redirect("#/home/");
     });
 
-    this.get("#/sorted-by/?:genre", function () {
+    this.get("#/sorted-by/?:genre", function() {
         let genre = this.params["genre"];
 
         galleyControl.getMoviesByGenre(genre)
-            .then(function (movies) {
+            .then(function(movies) {
                 template.get("home")
-                    .then(function (template) {
+                    .then(function(template) {
                         $content.html(template(movies));
                     });
             });
     });
 
-    this.get("#/contact/", function (context) {
+    this.get("#/top-rated", function() {
+        galleyControl.getMoviesByRate()
+            .then(function(movies) {
+                template.get("home")
+                    .then(function(template) {
+                        $content.html(template(movies));
+                    });
+            });
+    });
+
+    this.get("#/now-playing", function() {
+        galleyControl.getMoviesByNowPlaying()
+            .then(function(movies) {
+                template.get("home")
+                    .then(function(template) {
+                        $content.html(template(movies));
+                    });
+            });
+    });
+
+    this.get("#/contact/", function(context) {
         template.get("contacts")
-            .then(function (template) {
+            .then(function(template) {
                 $content.html(template());
             });
     });
