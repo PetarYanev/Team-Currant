4a022a255c881b31bbc76022d2e9e8f6c92683b2
PetarYanev <yanev.petar@gmail.com>
2017-05-01 22:39:40 +0300

    add more info in movies-info

--- Movies Finder/scripts/controllers/gallery-controller.js
+++ Movies Finder/scripts/controllers/gallery-controller.js
@@ -74,7 +74,7 @@ let galleyControl = function() {
         let promise = new Promise(function(resolve) {
 
             $.ajax({
-                url: "https://baas.kinvey.com/appdata/kid_HkCptq2Ae/movies/?query={}&sort={\"rate\": 1}",
+                url: "https://baas.kinvey.com/appdata/kid_HkCptq2Ae/movies/?query={}&sort={\"rate\": -1}",
                 method: "GET",
                 headers: {
                     "Authorization": "Basic a2lkX0hrQ3B0cTJBZTo3OWY0ZmIwODE4MmU0NmMxOTBlNTkzNWYzNzEyZDQ3Mw=="
