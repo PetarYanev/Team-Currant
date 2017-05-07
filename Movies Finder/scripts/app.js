f673fb1596fdca2e344d0a2adc1a13c9e0dc3789
SimonaArsova <simona.liubomirova@gmail.com>
2017-05-07 11:14:47 +0300

    Remove the Watch trailer button

--- Movies Finder/scripts/app.js
+++ Movies Finder/scripts/app.js
@@ -4,7 +4,7 @@ import { template } from "template";
 import { accountControl } from "account-controller";
 import { galleyControl } from "gallery-controller";
 
-let router = Sammy("#content", function() {
+let router = Sammy("#content", function () {
     let $content = $("#content");
 
     if (accountControl.currentUser()) {
@@ -19,23 +19,23 @@ let router = Sammy("#content", function() {
         $("#nav-btn-logout").addClass("hidden");
     }
 
-    this.get("#/home/", function() {
+    this.get("#/home/", function () {
         let skip = 0;
         let moviesInDB;
 
         galleyControl.getAllMovies(skip)
-            .then(function(movies) {
+            .then(function (movies) {
                 template.get("home")
-                    .then(function(template) {
+                    .then(function (template) {
                         $content.html(template(movies));
                     });
             });
         galleyControl.getMoviesCount()
-            .then(function(moviesCount) {
+            .then(function (moviesCount) {
                 moviesInDB = moviesCount.count | 0;
             });
 
-        $(document).scroll(function() {
+        $(document).scroll(function () {
             if ($(window).scrollTop() == $(document).height() - $(window).height()) {
                 if (window.location.hash === "#/home/") {
                     loadNewData();
@@ -51,9 +51,9 @@ let router = Sammy("#content", function() {
             if (remainingMovies >= 0) {
                 skip += 8;
                 galleyControl.getAllMovies(skip)
-                    .then(function(movies) {
+                    .then(function (movies) {
                         template.get("home")
-                            .then(function(template) {
+                            .then(function (template) {
                                 $content.append(template(movies));
                             });
                     });
@@ -61,24 +61,24 @@ let router = Sammy("#content", function() {
         }
     });
 
-    this.get("#/login", function(context) {
+    this.get("#/login", function (context) {
         if (accountControl.currentUser()) {
             context.redirect("#/home/");
             return;
         }
 
         template.get("login")
-            .then(function(template) {
+            .then(function (template) {
                 $content.html(template());
 
-                $("#btn-login").on("click", function() {
+                $("#btn-login").on("click", function () {
                     let loginUser = {
                         username: $("#tb-username").val(),
                         password: $("#tb-password").val()
                     };
 
                     accountControl.userLogin(loginUser)
-                        .then(function() {
+                        .then(function () {
                             context.redirect("#/home/");
                             $("#nav-btn-logout").removeClass("hidden");
                             $("#nav-btn-login").addClass("hidden");
@@ -89,24 +89,24 @@ let router = Sammy("#content", function() {
             });
     });
 
-    this.get("#/register", function(context) {
+    this.get("#/register", function (context) {
         if (accountControl.currentUser()) {
             context.redirect("#/login");
             return;
         }
 
         template.get("register")
-            .then(function(template) {
+            .then(function (template) {
                 $content.html(template());
 
-                $("#btn-register").on("click", function() {
+                $("#btn-register").on("click", function () {
                     let registerNewUser = {
                         username: $("#tb-regUsername").val(),
                         password: $("#tb-regPassword").val()
                     };
 
                     accountControl.userRegister(registerNewUser)
-                        .then(function() {
+                        .then(function () {
                             if (registerNewUser.username.trim() === "" || registerNewUser.password.trim() === "") {
                                 // toastr.error("Invalid username or password");
                             } else {
@@ -117,7 +117,7 @@ let router = Sammy("#content", function() {
             });
     });
 
-    this.get("#/logout", function(context) {
+    this.get("#/logout", function (context) {
         accountControl.userLogout();
         context.redirect("#/home/");
         $("#nav-btn-logout").addClass("hidden");
@@ -126,58 +126,58 @@ let router = Sammy("#content", function() {
         $("#my-watchlist").addClass("hidden");
     });
 
-    this.get("#/sorted-by/?:genre", function() {
+    this.get("#/sorted-by/?:genre", function () {
         let genre = this.params["genre"];
 
         galleyControl.getMoviesByGenre(genre)
-            .then(function(movies) {
+            .then(function (movies) {
                 template.get("home")
-                    .then(function(template) {
+                    .then(function (template) {
                         $content.html(template(movies));
                     });
             });
     });
 
-    this.get("#/top-rated", function() {
+    this.get("#/top-rated", function () {
         galleyControl.getMoviesByRate()
-            .then(function(movies) {
+            .then(function (movies) {
                 template.get("home")
-                    .then(function(template) {
+                    .then(function (template) {
                         $content.html(template(movies));
                     });
             });
     });
 
-    this.get("#/now-playing", function() {
+    this.get("#/now-playing", function () {
         galleyControl.getMoviesByNowPlaying()
-            .then(function(movies) {
+            .then(function (movies) {
                 template.get("home")
-                    .then(function(template) {
+                    .then(function (template) {
                         $content.html(template(movies));
                     });
             });
     });
 
-    this.get("#/movies-info/?:name", function() {
+    this.get("#/movies-info/?:name", function () {
         let title = this.params["name"];
 
         galleyControl.getMoviesByTitle(title)
-            .then(function(movie) {
+            .then(function (movie) {
                 template.get("movies-info")
-                    .then(function(template) {
+                    .then(function (template) {
                         $content.html(template(movie));
                     });
             });
     });
 
-    this.get("#/search", function() {
-        $("#search-btn").on("click", function() {
+    this.get("#/search", function () {
+        $("#search-btn").on("click", function () {
 
             let title = $("#search").val();
             let moviesToShow = [];
 
             galleyControl.getAllMovies()
-                .then(function(movies) {
+                .then(function (movies) {
                     for (let i = 0; i < movies.length; i += 1) {
                         if (movies[i].name.toLowerCase().indexOf(title.toLowerCase()) >= 0) {
                             moviesToShow.push(movies[i]);
@@ -185,27 +185,27 @@ let router = Sammy("#content", function() {
                     }
 
                     template.get("home")
-                        .then(function(template) {
+                        .then(function (template) {
                             $content.html(template(moviesToShow));
                         });
                 });
         });
     });
 
-    this.get("#/watchlist/?:name", function() {
+    this.get("#/watchlist/?:name", function () {
         let title = this.params["name"];
 
         galleyControl.getMoviesByTitle(title)
-            .then(function(movie) {
+            .then(function (movie) {
                 accountControl.getMoviesFromUsersWatchlist()
-                    .then(function(user) {
+                    .then(function (user) {
                         let movies = user.watchlist;
                         let isTheMovieAdded = false;
 
                         if (!movies) {
                             accountControl.addMovieToUserWatchlist(movie);
                         } else {
-                            movies.forEach(function(movieInWatchlist) {
+                            movies.forEach(function (movieInWatchlist) {
                                 if (movieInWatchlist._id === movie[0]._id) {
                                     isTheMovieAdded = true;
                                 }
@@ -221,11 +221,11 @@ let router = Sammy("#content", function() {
             });
     });
 
-    this.get("#/watchlist-remove/?:name", function(context) {
+    this.get("#/watchlist-remove/?:name", function (context) {
         let title = this.params["name"];
 
         accountControl.getMoviesFromUsersWatchlist()
-            .then(function(user) {
+            .then(function (user) {
                 let movies = user.watchlist;
 
                 for (var i = 0; i < movies.length; i += 1) {
@@ -242,18 +242,18 @@ let router = Sammy("#content", function() {
     });
 
 
-    this.get("#/contact/", function() {
+    this.get("#/contact/", function () {
         template.get("contacts")
-            .then(function(template) {
+            .then(function (template) {
                 $content.html(template());
             });
     });
 
-    this.get("#/my-watchlist", function() {
+    this.get("#/my-watchlist", function () {
         accountControl.getMoviesFromUsersWatchlist()
-            .then(function(movies) {
+            .then(function (movies) {
                 template.get("watchlist")
-                    .then(function(template) {
+                    .then(function (template) {
                         $content.html(template(movies.watchlist));
                     });
             });
