353dd83d01bb98f9d21724015a38f45759383914
PetarYanev <yanev.petar@gmail.com>
2017-05-08 00:15:36 +0300

    rawgit bug fixed

--- Movies Finder/scripts/configs/system-config.js
+++ Movies Finder/scripts/configs/system-config.js
@@ -5,18 +5,18 @@ SystemJS.config({
     transpiler: "plugin-babel",
     // tell SystemJS where to look for the dependencies
     map: {
-        "plugin-babel": "../node_modules/systemjs-plugin-babel/plugin-babel.js",
-        "systemjs-babel-build": "../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js",
+        "plugin-babel": "./node_modules/systemjs-plugin-babel/plugin-babel.js",
+        "systemjs-babel-build": "./node_modules/systemjs-plugin-babel/systemjs-babel-browser.js",
         // app start script
         "app": "./scripts/app.js",
-        "sammy": "../node_modules/sammy/lib/sammy.js",
-        "jquery": "../node_modules/jquery/dist/jquery.min.js",
-        "handlebars": "../node_modules/handlebars/dist/handlebars.js",
-        "cryptojs": "../node_modules/crypto-js/crypto-js.js",
-        "mocha": "../node_modules/mocha/lib/mocha.js",
-        "chai": "../node_modules/chai/chai.js",
-        "sinon": "../node_modules/sinon/pkg/sinon-2.2.0.js",
-        "sinon-chai": "../node_modules/sinon-chai/lib/sinon-chai.js",
+        "sammy": "./node_modules/sammy/lib/sammy.js",
+        "jquery": "./node_modules/jquery/dist/jquery.min.js",
+        "handlebars": "./node_modules/handlebars/dist/handlebars.js",
+        "cryptojs": "./node_modules/crypto-js/crypto-js.js",
+        "mocha": "./node_modules/mocha/lib/mocha.js",
+        "chai": "./node_modules/chai/chai.js",
+        "sinon": "./node_modules/sinon/pkg/sinon-2.2.0.js",
+        "sinon-chai": "./node_modules/sinon-chai/lib/sinon-chai.js",
 
         "template": "./scripts/template.js",
         "account-controller": "./scripts/controllers/account-controller.js",
