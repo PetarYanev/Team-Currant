11366e8361c994f8c1f2dbcdb2d2622494dd04d5
Martin Kantarski <kantarskim@gmail.com>
2017-05-09 05:47:38 +0300

    Add same tests

--- Movies Finder/tests/test.js
+++ Movies Finder/tests/test.js
@@ -6,41 +6,202 @@ mocha.setup('bdd');
 
 let expect = chai.expect;
 
-describe("Login tests", () => {
-    let requesterStub;
-    let encryptorStub;
+describe('Data test', ()=>{
+    const LOCAL_STORAGE_USERNAME_KEY = 'username',
+        LOCAL_STORAGE_AUTHKEY_KEY = 'usernameKey';
 
-    beforeEach(() => {
-        requesterStub = sinon.stub(requester, "post");
-        encryptorStub = sinon.stub(encryptor, 'encryptToSha1')
-            .returns('SOME_PASS_HASH');
-    });
+    const clearLocalStorage = () => {
+        localStorage.removeItem(LOCAL_STORAGE_USERNAME_KEY);
+        localStorage.removeItem(LOCAL_STORAGE_AUTHKEY_KEY);
+    };
 
-    afterEach(() => {
-        requesterStub.restore();
-        encryptorStub.restore();
-    });
+    beforeEach(clearLocalStorage);
+    afterEach(clearLocalStorage);
 
-    it("expect login to call to encryptor.encryptToSha1 once", (done) => {
-        const user = {
-            username: "someUser",
-            password: "somePass234"
-        };
 
-        const response = {
-            result: {
-                username: user.username,
-                authKey: "SOME_AUTH_KEY"
-            }
-        };
+    describe("User test", () => {
+        let requesterStub;
+        let encryptorStub;
+        const passHash = 'SOME_PASS_HASH';
 
-        requesterStub.returns(Promise.resolve(response));
+        beforeEach(() => {
+            requesterStub = sinon.stub(requester, "post");
+            encryptorStub = sinon.stub(encryptor, 'encryptToSha1')
+                .returns(passHash);
+        });
 
-        accountData.userLogin(user)
-            .then(() => {
-                expect(encryptorStub).to.have.been.calledOnce;
-            })
-            .then(done, done);
+        afterEach(() => {
+            requesterStub.restore();
+            encryptorStub.restore();
+        });
+
+        it("expect login to call to encryptor.encryptToSha1 once", (done) => {
+            const user = {
+                username: "someUser",
+                password: "somePass234"
+            };
+
+            const response = {
+                result: {
+                    username: user.username,
+                    authKey: "SOME_AUTH_KEY"
+                }
+            };
+
+            requesterStub.returns(Promise.resolve(response));
+
+            accountData.userLogin(user)
+                .then(() => {
+                    expect(encryptorStub).to.have.been.calledOnce;
+                })
+                .then(done, done);
+        });
+
+        it('expect register to make a POST request', (done)=>{
+            const user = {
+                username: "someUser",
+                password: "somePass234"
+            };
+
+            const response = {
+                result: {
+                    username: user.username,
+                    authKey: "SOME_AUTH_KEY"
+                }
+            };
+            requesterStub.returns(Promise.resolve(response));
+
+            accountData.userRegister(user)
+                .then(()=>{
+                    expect(requesterStub).to.have.been.calledOnce;
+                })
+                .then(done, done);
+        });
+
+        it('expect username to be set in localStorage', (done)=>{
+            const user = {
+                username: "someUser",
+                password: "somePass234"
+            };
+
+            const response = {
+                result: {
+                    username: user.username,
+                    authKey: "SOME_AUTH_KEY"
+                }
+            };
+
+            localStorage.setItem('username', user.username);
+            requesterStub.returns(Promise.resolve(response));
+
+            accountData.userRegister(user)
+                .then(()=>{
+                    expect(localStorage.getItem('username')).to.equal(user.username);
+                })
+                .then(done, done);
+            clearLocalStorage;
+        });
+
+        it('expect authKey to be set in localStorage', (done)=>{
+            const user = {
+                username: "someUser",
+                password: "somePass234"
+            };
+
+            const response = {
+                result: {
+                    username: user.username,
+                    authKey: "SOME_AUTH_KEY"
+                }
+            };
+
+            localStorage.setItem('usernameKey', response.result.authKey);
+            requesterStub.returns(Promise.resolve(response));
+
+            accountData.userRegister(user)
+                .then(()=>{
+                    expect(localStorage.getItem('usernameKey')).to.equal(response.result.authKey);
+                })
+                .then(done, done);
+            clearLocalStorage;
+        });
+
+        // it('expect register function to return a Promise which resolves with registered username', (done)=>{
+        //     const user = {
+        //         username: "someUser",
+        //         password: "somePass234"
+        //     };
+
+        //     const response = {
+        //         result: {
+        //             username: user.username,
+        //             authKey: "SOME_AUTH_KEY"
+        //         }
+        //     };
+        //     requesterStub.returns(Promise.resolve(response));
+
+        //     accountData.userRegister(user)
+        //         .then((value)=>{
+        //             const expected = {
+        //                 username: user.username
+        //             };
+        //             expect(value).to.deep.equal(expected);
+        //         })
+        //         .then(done, done);
+        // });
+
+        
+        // it('expect register to make a POST request to (url, headers, user)', (done)=>{
+        //     const user = {
+        //         username: "someUser",
+        //         password: "somePass234"
+        //     };
+        //     const authorization = encryptor.encryptToBase64("kid_HkCptq2Ae:f78eee25f64842e28ddda28312edac4a");
+        //     const url = "https://baas.kinvey.com/user/kid_HkCptq2Ae";
+        //     const headers = {
+        //             "Authorization": `Basic ${authorization}`
+        //         }
+
+        //     const response = {
+        //         result: {
+        //             username: user.username,
+        //             authKey: "SOME_AUTH_KEY"
+        //         }
+        //     };
+        //     requesterStub.returns(Promise.resolve(response));
+
+        //     accountData.userRegister(user)
+        //         .then(()=>{
+        //             expect(requesterStub).to.have.been.calledWith(url, headers, user);
+        //         })
+        //         .then(done, done);
+        // });
+
+        // it('expect register to make a POST request with user data (username)', (done)=>{
+        //     const user = {
+        //         username: "someUser",
+        //         password: "somePass234"
+        //     };
+
+        //     const response = {
+        //         result: {
+        //             username: user.username,
+        //             authKey: "SOME_AUTH_KEY"
+        //         }
+        //     };
+        //     requesterStub.returns(Promise.resolve(response));
+
+        //     accountData.userRegister(user)
+        //         .then(()=>{
+        //             const expected = {
+        //                 data : {
+        //                     username: user.username
+        //                     }
+        //             };
+        //             expect(requesterStub.args[0][1].data.username).to.equal(user.username);
+        //         })
+        //         .then(done, done);
+        // });
     });
 });
 
